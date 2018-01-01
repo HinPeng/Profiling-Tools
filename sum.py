b422aca66fb5aaf890495a55a3e44cfd2aa08b11
Xuan Peng (MSR Student-FA Talent) <v-xuapen@microsoft.com>
2018-01-01 21:52:06 +0800

    update

--- sum.py
+++ sum.py
@@ -42,7 +42,7 @@ def cpu_mem(in_p, out_p):
         mem.append(float(tokens[2]))
 
     out_p.write('cpu average usage: '+str(sum(cpu)/len(cpu))+'\n')
-    out_p.write('memory max usage:'+str(max(mem)*515880/102400)+'\n')
+    out_p.write('memory max usage: '+str(max(mem)*515880/102400)+'\n')
 
     return
 
@@ -68,21 +68,44 @@ def smi(in_p, out_p):
             gpu.append(float(tokens[2]))
 
 
+    start=int(0.1*len(gpu))
+    gpu=gpu[start:]
+    tx_t=tx_t[start:]
+    rx_t=rx_t[start:]
 
     out_p.write("GPU average usage: "+str(sum(gpu)/len(gpu))+"\n")
     out_p.write("GPU average tx throughput: "+str(sum(tx_t)/len(tx_t)/1024)+"\n")
-    out_p.write("GPU average rx usage: "+str(sum(rx_t)/len(rx_t)/1024)+"\n")
+    out_p.write("GPU average rx throughput: "+str(sum(rx_t)/len(rx_t)/1024)+"\n")
         
             
     return
 
-filename=sys.argv[1]
+def disk(in_p, out_p):
+    d_read=[]
+    d_write=[]
+    d_util=[]
+
+    lines=in_p.readlines()
+    for line in lines:
+        if 'sda' in line:
+            tokens=line.split()
+            d_read.append(float(tokens[5])/1024)
+            d_write.append(float(tokens[6])/1024)
+            d_util.append(float(tokens[13]))
+
+    out_p.write("Disk average reading bandwidth: "+str(sum(d_read)/len(d_read))+"\n")
+#    out_p.write("Disk average writing bandwidth: "+str(sum(d_write)/len(d_write))+"\n")
+    out_p.write("Disk average utilization: "+str(sum(d_util)/len(d_util))+"\n")
+
+    return
+
+full_filename=sys.argv[1]
 out_filename='sum_result.txt'
 
 in_p=open(filename, 'r')
 out_p=open(out_filename, 'a+')
 
-filename=filename[5:]
+filename=full_filename.split('/')[-1]
 tokens=filename.split('_')
 out_p.write(tokens[0]+'\t'+tokens[1]+'\t'+tokens[2]+'\n')
 
@@ -90,6 +113,7 @@ out_p.write(tokens[0]+'\t'+tokens[1]+'\t'+tokens[2]+'\n')
 
 if tokens[3] == 'cpu':
     cpu_mem(in_p, out_p)
-#elif tokens[3] == 'pcm':
 elif tokens[3] == 'smi.txt':
     smi(in_p, out_p)
+elif tokens[3] == 'io.txt':
+    disk(in_p, out_p)
