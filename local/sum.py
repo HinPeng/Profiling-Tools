537365b10b19b40b2a30e2dcb280a59b0a9ea7e7
HinPeng <pieceseix@gmail.com>
2018-01-14 22:22:57 +0800

    Update sum.py

--- local/sum.py
+++ local/sum.py
@@ -1,6 +1,6 @@
 import sys
 
-def cpu_mem(in_p, out_p):
+def cpu_mem(in_p, out_p, start, end):
     cpu=[]
     mem=[]
 
@@ -9,6 +9,9 @@ def cpu_mem(in_p, out_p):
         tokens=line.split()
         cpu.append(float(tokens[1]))
         mem.append(float(tokens[2]))
+
+    cpu=cpu[start:end]
+    mem=mem[start:end]
 
     out_p.write('cpu average usage: '+str(sum(cpu)/len(cpu))+'\n')
     out_p.write('memory max usage: '+str(max(mem)*515880/102400)+'\n')
@@ -26,7 +29,7 @@ def smi(in_p, out_p):
     lines=in_p.readlines()
     for index, line in enumerate(lines):
         if "Bus Id" in line:
-            continue    # Ignore the GPU id.
+            continue    # Ignore GPU id.
 
         if "Used GPU Memory" in line:
             tag.append(index)
@@ -39,9 +42,11 @@ def smi(in_p, out_p):
             elif "Gpu" in line:
                 gpu.append(float(tokens[2]))
 
-
-    start=(tag[0]-1)/4
-    end=len(tag)
+    #hack_num=len(tag) * 0.2
+    start=tag[0]/4 + int(len(tag)*0.1)
+    end=int(len(tag)*0.8) + start
+    #start=tag[0]/4
+    #end=int(len(tag)) + start
     gpu=gpu[start:end]
     tx_t=tx_t[start:end]
     rx_t=rx_t[start:end]
@@ -49,11 +54,10 @@ def smi(in_p, out_p):
     out_p.write("GPU average usage: "+str(sum(gpu)/len(gpu))+"\n")
     out_p.write("GPU average tx throughput: "+str(sum(tx_t)/len(tx_t)/1024)+"\n")
     out_p.write("GPU average rx throughput: "+str(sum(rx_t)/len(rx_t)/1024)+"\n")
-        
-            
-    return
+                    
+    return start,end
 
-def disk(in_p, out_p):
+def disk(in_p, out_p, start, end):
     d_read=[]
     d_write=[]
     d_util=[]
@@ -66,27 +70,35 @@ def disk(in_p, out_p):
             d_write.append(float(tokens[6])/1024)
             d_util.append(float(tokens[13]))
 
+    #Not work for I/O, due to prefetching?
+    #d_read=d_read[start:end]
+    #d_util=d_util[start:end]
+
     out_p.write("Disk average reading bandwidth: "+str(sum(d_read)/len(d_read))+"\n")
 #    out_p.write("Disk average writing bandwidth: "+str(sum(d_write)/len(d_write))+"\n")
     out_p.write("Disk average utilization: "+str(sum(d_util)/len(d_util))+"\n")
 
     return
 
-full_filename=sys.argv[1]
-out_filename='sum_result.txt'
+model=sys.argv[1]
+batch_size=sys.argv[2]
+cuda_devices=sys.argv[3]
 
-in_p=open(full_filename, 'r')
-out_p=open(out_filename, 'w')
+dir_prefix="data/"
+prefix=dir_prefix+model+'_'+batch_size+'_'+cuda_devices+'_'
+cpu_file=open(prefix+'cpu_mem.txt', 'r')
+smi_file=open(prefix+'smi.txt', 'r')
+io_file=open(prefix+'io.txt', 'r')
 
-filename=full_filename.split('/')[-1]
-tokens=filename.split('_')
-out_p.write(tokens[0]+'\t'+tokens[1]+'\t'+tokens[2]+'\n')
+out_filename=model+'_result.txt'
+out_p=open(out_filename, 'a')
+out_p.write(model+'_'+batch_size+'_'+cuda_devices+'\n')
 
-#elif tokens[3] == 'pcm':
 
-if tokens[3] == 'cpu':
-    cpu_mem(in_p, out_p)
-elif tokens[3] == 'smi.txt':
-    smi(in_p, out_p)
-elif tokens[3] == 'io.txt':
-    disk(in_p, out_p)
+#filename=full_filename.split('/')[-1]
+#tokens=filename.split('_')
+#out_p.write(tokens[0]+'\t'+tokens[1]+'\t'+tokens[2]+'\n')
+tu=smi(smi_file, out_p)
+
+cpu_mem(cpu_file, out_p, tu[0], tu[1])
+disk(io_file, out_p, tu[0], tu[1])
